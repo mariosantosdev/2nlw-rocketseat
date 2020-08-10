26e3a20fbb0656d65a8e442ecf73c4479b615c31
nevr001 <playcar46@gmail.com>
2020-08-10 04:14:38 -0300

    Aula 4 finalizada
    Finalizamos a aplicação mobile, foi feito nessa aula a conexão do back end com o front end utilizando axios

--- mobile/src/screens/Favorites/index.tsx
+++ mobile/src/screens/Favorites/index.tsx
@@ -1,14 +1,46 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, ScrollView } from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage'
+import { useFocusEffect } from '@react-navigation/native'
 
 import PageHeader from '../../components/PageHeader'
 
 import styles from './styles'
+import TeacherItem, { ITeacher } from '../../components/TeacherItemx'
 
 const FavoritePage = () => {
+    const [favorites, setFavorites] = useState<number[]>([])
+    
+    function loadFavorite() {
+        AsyncStorage.getItem('favorites')
+            .then(res => {
+                if (res) {
+                    const teachers = JSON.parse(res)
+                    console.log(teachers)
+                    // setFavorites(teachers)
+                }
+            })
+    }
+
+    useFocusEffect(() => {
+        loadFavorite()
+    })
+
     return(
         <View style={styles.container}>
             <PageHeader title="Professores favoritos" />
+            <ScrollView style={styles.teacherList}
+                contentContainerStyle={{
+                    paddingHorizontal: 16,
+                    paddingBottom: 16
+                }}
+            >
+                {favorites.map((teacher: ITeacher) => {
+                    return (
+                        <TeacherItem key={teacher.id} teacher={teacher} favorited={true} />
+                    )
+                })}
+            </ScrollView>
         </View>
     )
 }
